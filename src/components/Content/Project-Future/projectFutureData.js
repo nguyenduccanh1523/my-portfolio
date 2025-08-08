eff4bafc71f70b25971884740304ee8391e49e92
Nguyễn Đức Cảnh <nguyenduccanh2398@gmail.com>
2025-08-09 04:49:49 +0700

    Implement code changes to enhance functionality and improve performance

--- src/components/Content/Project-Future/projectFutureData.js
+++ src/components/Content/Project-Future/projectFutureData.js
@@ -1,6 +1,7 @@
 // src/components/Content/Project-Future/projectFutureData.js
 import petshop from "../../../assets/react/petshop.png";
 import gara from "../../../assets/react/gara.jpg";
+import zuntie from "../../../assets/react/zuntie.jpg";
 import { projectFutureTexts } from "./projectFutureTexts";
 
 export const getProjectFutureData = (lang) => [
@@ -10,6 +11,13 @@ export const getProjectFutureData = (lang) => [
     description: projectFutureTexts.petshop[lang].description,
     link: "https://example.com/design-project-1",
     github: "https://github.com/nguyenduccanh1523/pet-shop",
+  },
+  {
+    title: projectFutureTexts.zuntie[lang].title,
+    image: zuntie,
+    description: projectFutureTexts.zuntie[lang].description,
+    link: "https://example.com/marketing-campaign",
+    github: "https://github.com/nguyenduccanh1523/auto-garage",
   },
   {
     title: projectFutureTexts.garaoto[lang].title,
--- src/components/Content/Project-Future/projectFutureTexts.js
+++ src/components/Content/Project-Future/projectFutureTexts.js
@@ -20,5 +20,15 @@ export const projectFutureTexts = {
       description: "Cung cấp một nền tảng trực tuyến cho khách hàng có thể: Đặt lịch dọn dẹp vệ sinh xe nhanh chóng. Xem và mua các loại nội thất ô tô phù hợp với nhiều dòng xe. Tùy chọn thiết kế và nâng cấp nội thất theo sở thích.",
     },
   },
+  zuntie: {
+    en: {
+      title: "ZUNTIE – Where ideas and brands connect about labels and printing",
+      description: "Explore our extensive collection of label templates, custom designs and easy ordering at ZUNTIE. From fashion to food, we help you create a unique brand identity with quality and inspiring labels.",
+    },
+    vi: {
+      title: "ZUNTIE – Nơi kết nối ý tưởng và nhãn mác thương hiệu",
+      description: "Khám phá kho mẫu nhãn mác đa dạng, thiết kế theo yêu cầu, và đặt hàng dễ dàng tại ZUNTIE. Từ thời trang đến thực phẩm, chúng tôi giúp bạn tạo nên dấu ấn thương hiệu riêng biệt với nhãn mác độc đáo, chất lượng và đầy cảm hứng.",
+    },
+  },
   
 };
